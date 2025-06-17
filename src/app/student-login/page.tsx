7b858392906e0ca7a2b2b1af9b9d3da0663d2d37
Mohit Kansana <mohitkansana82@gmail.com>
2025-06-17 09:37:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/student-login/page.tsx
+++ src/app/student-login/page.tsx
@@ -1,4 +1,1 @@
 
-// This file is intentionally left blank as it has been removed.
-// Student login functionality is no longer part of the application.
-// Admin access is managed via a localStorage key 'ADMIN_LOGGED_IN_KEY'.
