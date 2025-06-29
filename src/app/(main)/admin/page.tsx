94f1ad5fe91e9dfbfac9cacc73b927097979df43
Mohit Kansana <mohitkansana82@gmail.com>
2025-06-29 13:15:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(main)/admin/page.tsx
+++ src/app/(main)/admin/page.tsx
@@ -14,6 +14,7 @@ import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
 import { Switch } from '@/components/ui/switch';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Separator } from '@/components/ui/separator';
 import {
     addLiveClass, deleteLiveClass, getLiveClasses,
     addNotification, deleteNotification, getNotifications,
